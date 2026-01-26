46c92a3dc5793a7583b2052bc289c4109e28d57b
ali vaezi <ali.vaezi@studenti.polito.it>
2026-01-26 12:55:41 +0100

    Update service.py

--- src/services/thingspeak_adaptor/service.py
+++ src/services/thingspeak_adaptor/service.py
@@ -1,10 +1,11 @@
 import sys
 import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'common'))
 from MyMQTT import MyMQTT
 import time
 import requests
 import json
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'common'))
+
 
 class ThingSpeakAdaptor:
     """
